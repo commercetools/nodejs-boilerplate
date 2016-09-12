d31b577956ffb4a307d3ba4f56d8b5afc3b8f1c8
Abimbola Idowu <abimbola2010@gmail.com>
2016-09-12 11:53:55 +0200

    feat(TEST): add eslint, travis, istanbul
    install commercetools eslint configurations
    
     add isparta for code coverage
    
     add test scenario

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,13 +1,13 @@
 import test from 'tape'
 import addTwoNumbers from './../src/main'
 
-test('main module is a function', t => {
+test('main module is a function', (t) => {
   t.equal(typeof addTwoNumbers, 'function')
 
   t.end()
 })
 
-test('addTwoNumbers return 5 for 2 and 3', t => {
+test('addTwoNumbers return 5 for 2 and 3', (t) => {
   t.equal(addTwoNumbers(2, 3), 5)
 
   t.end()
