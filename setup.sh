19106bbf62d7e8ee7a40f9dde2dbee8c74e0891b
Selwyn <talk@selwyn.cc>
2016-09-16 17:35:24 +0200

    fix(template): improvements

--- setup.sh
+++ setup.sh
@@ -2,15 +2,20 @@
 # Prepare a "clean" installation of the boilerplate removing all boilerplate specific code.
 # Warning: running this script is destructive and will remove itself.
 
-echo $1
 # Overwrite the package.json with the template package.json
 mv package.template.json package.json
 mv README.template.md README.md
 
 # Setup fresh git history
-sudo rm -r .git
+rm -fr .git
 git init
 
-# Fill in project name
-sed -i "" "s/project-name/$1/g" "package.json"
+# Fill in project name if argument is given
+if [ $1 ]; then
+    sed -i "" "s/project-name/$1/g" "package.json"
+    echo "Set project name to $1."
+fi
+
+# Self destruct
+rm setup.sh
 
